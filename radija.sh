b9e98b5a95abcf1d53ec6b0530e5939c1f509f68
ritoj <atobiren@gmail.com>
2014-08-20 10:18:40 +0300

    pirmas upload

---
+++ radija.sh
@@ -0,0 +1,23 @@
+while true
+do
+
+# Kuri skaitoma eilute 'radija.txt' dokumente
+eilute=1
+eil_il=`wc -l < radija.txt`
+
+#echo "eilute"=$eilute
+#echo "eilutes_il"=$eil_il
+
+while [ $eil_il -ge $eilute ]
+do
+
+# Nuskaitoma dokumento eilute ir pakeiciamas jos skaicius
+mplayer `sed -n "$eilute"p radija.txt`
+
+eilute=$(($eilute + 1))
+sleep 1
+echo "eilute="$eilute
+done
+
+# Pagrindiio ciklo pabaiga
+done
