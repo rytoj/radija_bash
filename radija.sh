3db57755a748a3f89c921804ea9fe14ed058020c
rytoj <atobiren@gmail.com>
2014-09-25 01:20:45 +0300

    Lietuviskos radio stotys, pasirinkimas

--- radija.sh
+++ radija.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-# Version: 0.11
+# Version: 0.12
 # Programa skaito radijijo stotis is radija.txt
 # Sekanti stoti paleisti su: 'Ctrl + C' arba 'q'
+
+# Komanda numeruojanti radijas
+# $ nl -n rn radija.txt
+
+# Iskerpa tik pavadinimus
+# $ cut -d$'\t' -f2 radija.txt
+
+# Spalvinimas 
+#echo -e "\e[7m`ls pl/`\e[27m"
+#echo "---------------"
+#
+#
+# Pagrindinis grojarastis 'pl' kataloge
+grojarastis="lt.txt"
+
+# Atkerpa radio stoties pavadinima ir ji atspausdina
+cut -d$'\t' -f2 pl/lt.txt|nl -n rn
+
+# Atkerpa tik radio stoties nuoroda
+stotis=`cut -d$'\t' -f1 pl/lt.txt`
+
+echo $stoties_pav
+
+echo -e  "Pasirinkite: \c "
+
+read eilute
 
 while true
 do
 
 # Kuri skaitoma eilute 'radija.txt' dokumente
-eilute=1
-eil_il=`wc -l < radija.txt`
+
+eil_il=`wc -l < pl/$grojarastis`
 
 #echo "eilute"=$eilute
 #echo "eilutes_il"=$eil_il
@@ -18,11 +44,15 @@ while [ $eil_il -ge $eilute ]
 do
 
 # Nuskaitoma dokumento eilute ir pakeiciamas jos skaicius
-mplayer `sed -n "$eilute"p radija.txt`
+mplayer `sed -n "$eilute"p pl/$grojarastis`
 
 eilute=$(($eilute + 1))
 sleep 1
 echo "eilute="$eilute
 
 done # while ciklo pabaiga
+
+# Playlist nuolat kartojasi 
+#eilute=1
+
 done # pagrindinio ciklo pabaiga
